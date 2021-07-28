32f86f72f4ce537f58aaead7e8f0be7bdb80c37e
delta-kimigatame <sankaku.kuro@gmail.com>
2021-07-28 17:41:47 +0900

    Mode2からMode1に変換する際の初期化部分の作成

---
+++ utauPlugin/src/ConvertPitch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utauPlugin
+{
+    public partial class Note
+    {
+        public class ConvertPitch
+        {
+            private float msLength;
+            private int framePerPitch;
+            private float pitchPerMs;
+            private int pitchLength;
+
+            public float MsLength { get => msLength; }
+            public int FramePerPitch { get => framePerPitch; }
+            public float PitchPerMs { get => pitchPerMs; }
+            public int PitchLength { get => pitchLength; }
+
+            public ConvertPitch(Note note)
+            {
+                if (note.Next is null)
+                {
+                    msLength = note.GetMsLength() + note.GetAtPre() + note.GetAtStp();
+                }
+                else
+                {
+                    msLength = note.GetMsLength() + note.GetAtPre() + note.GetAtStp() + note.Next.GetAtOve() - note.Next.GetAtPre();
+                }
+                framePerPitch = (int)(60.0f / 96.0f / note.GetTempo() * 44100.0f + 0.5f);
+                pitchPerMs = framePerPitch / 44100.0f * 1000;
+                pitchLength = (int)(msLength / 1000f * 44100f / framePerPitch) + 1;
+            }
+            public List<int> Mode2Pitch2Mode1()
+            {
+                List<int> pitches = new List<int>();
+                return (pitches);
+            }
+
+        }
+    }
+}
---
+++ utauPlugin/test/test/ConvertPitchTest.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using utauPlugin;
+
+namespace ConvertPitchTest
+{
+    [TestClass]
+    public class SingleNoteInit
+    {
+        Note note;
+        Note.ConvertPitch cp;
+        [TestInitialize()]
+        public void testInitialize()
+        {
+            note = new Note();
+            note.SetLength("480");
+            note.SetLyric("あ");
+        }
+
+        [TestMethod]
+        public void InitParam()
+        {
+            note.InitTempo("120.0");
+            cp = new Note.ConvertPitch(note);
+            Assert.IsTrue(cp.MsLength == 500f);
+            Assert.IsTrue(cp.FramePerPitch == 230);
+            Assert.IsTrue(cp.PitchPerMs == 230 / 44100.0f * 1000);
+            Assert.IsTrue(cp.PitchLength == 96);
+        }
+        [TestMethod]
+        public void InitParam240()
+        {
+            note.InitTempo("240.0");
+            cp = new Note.ConvertPitch(note);
+            Assert.IsTrue(cp.MsLength == 250f);
+            Assert.IsTrue(cp.FramePerPitch == 115);
+            Assert.IsTrue(cp.PitchPerMs == 115 / 44100.0f * 1000);
+            Assert.IsTrue(cp.PitchLength == 96);
+        }
+        [TestMethod]
+        public void InitParamWithPre()
+        {
+            note.InitTempo("120.0");
+            note.SetAtPre(500);
+            cp = new Note.ConvertPitch(note);
+            Assert.IsTrue(cp.MsLength == 1000f);
+            Assert.IsTrue(cp.PitchLength == (int)(1000 / 1000 * 44100f / 230f) + 1);
+        }
+        [TestMethod]
+        public void InitParamWithStp()
+        {
+            note.InitTempo("120.0");
+            note.SetAtStp(500);
+            cp = new Note.ConvertPitch(note);
+            Assert.IsTrue(cp.MsLength == 1000f);
+            Assert.IsTrue(cp.PitchLength == (int)(1000 / 1000 * 44100f / 230f) + 1);
+        }
+    }
+    [TestClass]
+    public class MultiNoteInit
+    {
+        Note note1;
+        Note note2;
+        Note.ConvertPitch cp;
+        [TestInitialize()]
+        public void testInitialize()
+        {
+            note1 = new Note();
+            note1.SetLength("480");
+            note1.SetLyric("あ");
+            note1.InitTempo(120);
+            note2 = new Note();
+            note2.SetLength("480");
+            note2.SetLyric("あ");
+            note2.InitTempo(120);
+            note1.Next = note2;
+            note2.Prev = note1;
+        }
+        [TestMethod]
+        public void InitParam()
+        {
+            cp = new Note.ConvertPitch(note1);
+            Assert.IsTrue(cp.MsLength == 500f);
+            Assert.IsTrue(cp.FramePerPitch == 230);
+            Assert.IsTrue(cp.PitchPerMs == 230 / 44100.0f * 1000);
+            Assert.IsTrue(cp.PitchLength == 96);
+            cp = new Note.ConvertPitch(note2);
+            Assert.IsTrue(cp.MsLength == 500f);
+            Assert.IsTrue(cp.FramePerPitch == 230);
+            Assert.IsTrue(cp.PitchPerMs == 230 / 44100.0f * 1000);
+            Assert.IsTrue(cp.PitchLength == 96);
+        }
+        [TestMethod]
+        public void InitParamOtherTempo()
+        {
+            cp = new Note.ConvertPitch(note1);
+            Assert.IsTrue(cp.MsLength == 500f);
+            Assert.IsTrue(cp.FramePerPitch == 230);
+            Assert.IsTrue(cp.PitchPerMs == 230 / 44100.0f * 1000);
+            Assert.IsTrue(cp.PitchLength == 96);
+            note2.SetTempo(240);
+            cp = new Note.ConvertPitch(note2);
+            Assert.IsTrue(cp.MsLength == 250f);
+            Assert.IsTrue(cp.FramePerPitch == 115);
+            Assert.IsTrue(cp.PitchPerMs == 115 / 44100.0f * 1000);
+            Assert.IsTrue(cp.PitchLength == 96);
+        }
+        [TestMethod]
+        public void InitParamWithNextPre()
+        {
+            note2.SetAtPre(250);
+            cp = new Note.ConvertPitch(note1);
+            Assert.IsTrue(cp.MsLength == 250f);
+            Assert.IsTrue(cp.PitchLength == (int)(250 / 1000f * 44100f / 230f) + 1);
+        }
+        [TestMethod]
+        public void InitParamWithNextOve()
+        {
+            note2.SetAtOve(250);
+            cp = new Note.ConvertPitch(note1);
+            Assert.IsTrue(cp.MsLength == 750f);
+            Assert.IsTrue(cp.PitchLength == (int)(750 / 1000f * 44100f / 230f) + 1);
+        }
+        [TestMethod]
+        public void InitParamWithNextPreAndOve()
+        {
+            note2.SetAtPre(250);
+            note2.SetAtOve(250);
+            cp = new Note.ConvertPitch(note1);
+            Assert.IsTrue(cp.MsLength == 500f);
+            Assert.IsTrue(cp.PitchLength == (int)(500 / 1000f * 44100f / 230f) + 1);
+        }
+    }
+}
