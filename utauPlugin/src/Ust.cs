87f67219b51ea932ca51cd6fa564909488f8ae55
delta_kimigatame <sankaku.kuro@gmail.com>
2024-08-27 15:08:28 +0900

    ドキュメントコメントの追加

--- utauPlugin/src/Ust.cs
+++ utauPlugin/src/Ust.cs
@@ -3,35 +3,116 @@ using System.Collections.Generic;
 
 namespace UtauPlugin
 {
+    /// <summary>
+    /// UST(UTAU Sequence Text)ファイルを扱います。
+    /// </summary>
     public class Ust
     {
+        /// <summary>
+        /// ustファイルのフルパス
+        /// </summary>
         private string filePath;
+        /// <summary>
+        /// ustのバージョン
+        /// </summary>
         private string version;
+        /// <summary>
+        /// プロジェクト名
+        /// </summary>
         private string projectName;
+        /// <summary>
+        /// 音源フォルダのルート
+        /// </summary>
         private string voiceDir;
+        /// <summary>
+        /// キャッシュフォルダ
+        /// </summary>
         private string cacheDir;
+        /// <summary>
+        /// 出力するwavファイルのパス
+        /// </summary>
         private string outputFile;
+        /// <summary>
+        /// プロジェクトのBPM
+        /// </summary>
         private float tempo;
+        /// <summary>
+        /// tool1(wavtool)のパス
+        /// </summary>
         private string tool1Path;
+        /// <summary>
+        /// tool2(resamp)のパス
+        /// </summary>
         private string tool2Path;
+        /// <summary>
+        /// プロジェクトのフラグ
+        /// </summary>
         private string flags;
+        /// <summary>
+        /// mode2使用の有無
+        /// </summary>
         private Boolean mode2;
+        /// <summary>
+        /// ustの文字コードがutf8の場合true、ANSIの場合false
+        /// </summary>
         private Boolean utf8;
+        /// <summary>
+        /// ノートのリスト
+        /// </summary>
         public List<Note> note;
 
+        /// <summary>
+        /// ustファイルのフルパス
+        /// </summary>
         public string FilePath { get => filePath; set => filePath = value; }
+        /// <summary>
+        /// プロジェクト名
+        /// </summary>
         public string Version { get => version; set => version = value; }
+        /// <summary>
+        /// ustのバージョン
+        /// </summary>
         public string ProjectName { get => projectName; set => projectName = value; }
+        /// <summary>
+        /// 音源フォルダのルート
+        /// </summary>
         public string VoiceDir { get => voiceDir; set => voiceDir = value; }
+        /// <summary>
+        /// キャッシュフォルダ
+        /// </summary>
         public string CacheDir { get => cacheDir; set => cacheDir = value; }
+        /// <summary>
+        /// 出力するwavファイルのパス
+        /// </summary>
         public string OutputFile { get => outputFile; set => outputFile = value; }
+        /// <summary>
+        /// プロジェクトのBPM
+        /// </summary>
         public float Tempo { get => tempo; set => tempo = value; }
+        /// <summary>
+        /// tool1(wavtool)のパス
+        /// </summary>
         public string Tool1Path { get => tool1Path; set => tool1Path = value; }
+        /// <summary>
+        /// tool2(resamp)のパス
+        /// </summary>
         public string Tool2Path { get => tool2Path; set => tool2Path = value; }
+        /// <summary>
+        /// プロジェクトのフラグ
+        /// </summary>
         public string Flags { get => flags; set => flags = value; }
+        /// <summary>
+        /// mode2使用の有無
+        /// </summary>
         public bool Mode2 { get => mode2; set => mode2 = value; }
+        /// <summary>
+        /// ustの文字コードがutf8の場合true、ANSIの場合false
+        /// </summary>
         public bool Utf8 { get => utf8; set => utf8 = value; }
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         public Ust()
         {
             FilePath = "";
@@ -48,6 +129,16 @@ namespace UtauPlugin
             note = new List<Note>();
             Utf8 = false;
         }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <remarks>
+        /// 引数<c>filePath</c>は、コンストラクタ時点では使用されず<c>Ust.filePath</c>に格納されます。
+        /// </remarks>
+        /// <value>
+        /// <c>filePath</c>はwavファイルまでの絶対パス
+        /// </value>
         public Ust(string filePath)
         {
             this.FilePath = filePath;
@@ -64,36 +155,6 @@ namespace UtauPlugin
             note = new List<Note>();
             Utf8 = false;
         }
-
-        //public void SetFilePath(string filePath) => this.FilePath = filePath;
-        //public string GetFilePath() => FilePath;
-        //public void SetVersion(float version) => this.Version = version;
-        //public void SetVersion(string version) => this.Version = float.Parse(version);
-        //public float GetVersion() => Version;
-        //public void SetProjectName(string projectName) => this.ProjectName = projectName;
-        //public string GetProjectName() => ProjectName;
-        //public void SetVoiceDir(string voiceDir) => this.VoiceDir = voiceDir;
-        //public string GetVoiceDir() => VoiceDir;
-        //public void SetCacheDir(string cacheDir) => this.CacheDir = cacheDir;
-        //public string GetCacheDir() => CacheDir;
-        //public void SetOutputFile(string outputFile) => this.OutputFile = outputFile;
-        //public string GetOutputFile() => OutputFile;
-        //public void SetTempo(float tempo) => this.Tempo = tempo;
-        //public void SetTempo(string tempo) => this.Tempo = float.Parse(tempo);
-        //public float GetTempo() => Tempo;
-        //public void SetTool1Path(string tool1Path) => this.Tool1Path = tool1Path;
-        //public string GetTool1Pathe() => Tool1Path;
-        //public void SetTool2Path(string tool2Path) => this.Tool2Path = tool2Path;
-        //public string GetTool2Path() => Tool2Path;
-        //public void SetFlags(string flags) => this.Flags = flags;
-        //public string GetFlags() => Flags;
-        //public void SetMode2(Boolean mode2) => this.Mode2 = mode2;
-        //public void SetMode2(String mode2) => this.Mode2 = Boolean.Parse(mode2);
-        //public Boolean IsMode2() => Mode2;
-        //public void SetUtf8(Boolean utf8) => this.Utf8 = utf8;
-        //public void SetUtf8(String utf8) => this.Utf8 = Boolean.Parse(utf8);
-        //public Boolean IsUtf8() => Utf8;
-
 
     }
 }
