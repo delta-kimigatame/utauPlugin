661d4953a0c2f15c05a6311a70050676202856ee
delta_kimigatame <sankaku.kuro@gmail.com>
2024-08-27 16:53:28 +0900

    プロジェクト名変更、assemblyinfoの追加

--- utauPlugin/Properties/AssemblyInfo.cs
+++ utauPlugin/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("utauPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("UtauPlugin")]
+[assembly: AssemblyDescription("Class library for creating plug-ins for UTAU which voice synthesis software.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("utauPlugin")]
+[assembly: AssemblyCompany("Kimigatame")]
+[assembly: AssemblyProduct("UtauPlugin")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("ja-JP")]
