fbce78ff5169050d0e01546806277c4a872c88e9
delta_kimigatame <sankaku.kuro@gmail.com>
2024-09-01 11:53:28 +0900

    nuget化

--- utauPlugin/test/test/ConvertPitchTest.cs
+++ utauPlugin/test/test/ConvertPitchTest.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using utauPlugin;
+using UtauPlugin;
 
 namespace ConvertPitchTest
 {
--- utauPlugin/test/test/noteNumTest.cs
+++ utauPlugin/test/test/noteNumTest.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using utauPlugin;
+using UtauPlugin;
 
 namespace noteNumTest
 {
